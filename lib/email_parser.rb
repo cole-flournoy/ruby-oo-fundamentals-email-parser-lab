ce6c7cb5fa47dc897324882df21080623037d2b0
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-14 17:07:58 -0500

    commit tree 0793efe212d290d02ace286b6d44a9c4027e206a

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,24 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    attr_accessor :email_list
+    def initialize(email_addresses)
+        @emails = email_addresses
+    end
+    def parse
+        @emails = @emails.gsub(", ", " ").split
+        @emails = @emails.uniq
+    end
+end
+
+# email_addresses = "john@doe.com, person@somewhere.org yaboi@gmail.com"
+# parser = EmailAddressParser.new(email_addresses)
+
+# parser.parse
+
+# parser.email_list = "peep@fun.com, floop@fooglies.com test@bot.net"
+# parser.parse
+# parser.email_list
+
+# I don't understand the use of attr_accessor here ^this yields an error
